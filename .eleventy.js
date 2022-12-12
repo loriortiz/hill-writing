d74e0d631a5fa2cf238bc01b033725499adafbf0
lorio <lori@loriortiz.com>
2022-12-12 07:57:39 -0500

    Change icon to icns

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const excerpt = require('./plugins/excerpt');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/favicon.icns");
   eleventyConfig.addWatchTarget("./src/style.css");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.setFrontMatterParsingOptions({
